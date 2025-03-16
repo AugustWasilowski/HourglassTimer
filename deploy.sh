3c1644059c4380dce7bd274e0f59b6b068b96b60
Mayor Awesome <august.wasilowski@gmail.com>
2025-03-16 18:21:49 -0500

    fix docker config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Build the Docker image
-docker build -t hourglass-timer .
+# Build the Docker image with explicit context path
+docker build -t hourglass-timer:latest .
 
 # Run the container
-docker run -d -p 80:80 --name hourglass-timer hourglass-timer
+docker run -d -p 80:80 --name hourglass-timer hourglass-timer:latest
